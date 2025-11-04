e137f7405b8925fe99e6b170739f7627c61dd2ea
thanachot <su.thanachot@gmail.com>
2025-11-04 14:13:27 +0700

    [FIX] change to use StatusList2021 type

--- src/credentials-status/credentials-status.controller.ts
+++ src/credentials-status/credentials-status.controller.ts
@@ -14,12 +14,12 @@ export class CredentialsStatusController {
     const Status_List_Credential = {
       '@context': ['https://www.w3.org/ns/credentials/v2'],
       id: 'https://example.com/credentials/status/3',
-      type: ['VerifiableCredential', 'BitstringStatusListCredential'],
+      type: ['VerifiableCredential', 'StatusList2021Credential'],
       issuer: 'did:example:12345',
       validFrom: '2021-04-05T14:27:40Z',
       credentialSubject: {
         id: 'https://example.com/status/3#list',
-        type: 'BitstringStatusList',
+        type: 'StatusList2021',
         statusPurpose: 'revocation',
         encodedList: encodedList,
       },
